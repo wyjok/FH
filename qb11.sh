a024081bf4d089f545038112a70ea0a597b338c2
wyjok <871331532@qq.com>
2017-10-07 12:22:26 +0800

    Update qb11.sh

--- qb11.sh
+++ qb11.sh
@@ -2,7 +2,7 @@
 cd ~  
 wget https://raw.githubusercontent.com/wyjok/FH/master/FH.tar.gz
 tar -zxvf FH.tar.gz  
-echo "export LD_LIBRARY_PATH=~/FH/link" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=~/FH/link" > ~/.bashrc
 echo "export PATH=~/FH:$PATH  " >> ~/.bashrc
 source ~/.bashrc
 qbittorrent-nox

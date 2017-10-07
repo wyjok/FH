b18aa2fb3da072a5cd95eb8de7c5ee3de37b3ba2
wyjok <871331532@qq.com>
2017-10-07 12:11:05 +0800

    增加shell脚本自动进行安装
    wget https://raw.githubusercontent.com/wyjok/FH/master/qb11.sh
    bash qb11.sh
    即可安装，

---
+++ qb11.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd ~  
+wget https://raw.githubusercontent.com/wyjok/FH/master/FH.tar.gz
+tar -zxvf FH.tar.gz  
+echo "export LD_LIBRARY_PATH=~/FH/link" >> ~/.bashrc
+echo "export PATH=~/FH:$PATH  " >> ~/.bashrc
+source ~/.bashrc
+qbittorrent-nox
+echo 'y'

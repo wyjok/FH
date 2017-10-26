7c2c5c8056b4fb3a3b96b22f090d012953964666
wyjok <871331532@qq.com>
2017-10-27 00:03:53 +0800

    Update deluge1.3.13.sh

--- deluge1.3.13.sh
+++ deluge1.3.13.sh
@@ -4,4 +4,7 @@ cd *1.3.13
 export PYTHONPATH=~/mydeluge/lib/python2.7/site-packages/
 python setup.py install  --prefix=~/mydeluge
 export PATH=~/mydeluge:$PATH
-~/mydeluge/bin/deluge-web --fork --config ~/mydeconf --port 65500
+echo "export PATH=~/mydeluge:$PATH" >> ~/.bashrc
+source ~/.bashrc
+echo "use following Command to run deluge and add an new daemon connection with non-default port:"
+echo "deluge-web --fork --config ~/CONFIGPATH --port PORTNUMBER(you need to change the CONFIGPATH & PORTNUMBER)"

46d1d5fe54144d812129741a713099221ed45dd0
wyjok <871331532@qq.com>
2017-10-27 11:51:32 +0800

    Update deluge1.3.13.sh

--- deluge1.3.13.sh
+++ deluge1.3.13.sh
@@ -1,9 +1,10 @@
 wget http://download.deluge-torrent.org/source/deluge-1.3.13.tar.gz
 tar -zxvf deluge*gz 
 cd *1.3.13
+export PYTHONPATH=~/mydeluge
+python setup.py install  --prefix=~/mydeluge
 export PYTHONPATH=~/mydeluge/lib/python2.7/site-packages/
 python setup.py install  --prefix=~/mydeluge
-export PATH=~/mydeluge:$PATH
 echo "export PATH=~/mydeluge:$PATH" >> ~/.bashrc
 source ~/.bashrc
 echo "use following Command to run deluge and add an new daemon connection with non-default port:"
